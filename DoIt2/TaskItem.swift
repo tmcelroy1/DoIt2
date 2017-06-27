5441674f1688763d4fa7114e7dc5d6d430628972
Tim McElroy1 <tmcelroyt@mac.com>
2017-06-27 00:38:44 -0400

    Added Segue 1

---
+++ DoIt2/TaskItem.swift
@@ -0,0 +1,14 @@
+//
+//  TaskItem.swift
+//  DoIt2
+//
+//  Created by Tim McElroy1 on 6/27/17.
+//  Copyright © 2017 Tim McElroy1. All rights reserved.
+//
+
+import Foundation
+
+class TaskItem{
+    var name = ""
+    var imporant = false
+}
--- DoIt2/ViewController.swift
+++ DoIt2/ViewController.swift
@@ -8,19 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [TaskItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.imporant{
+            cell.textLabel?.text = "❗️\(task.name)"
+        } else{
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
+    }
+    
+    func makeTasks() -> [TaskItem]{
+        let task1 = TaskItem()
+        task1.name = "Walk the dog!"
+        task1.imporant = false
+        
+        let task2 = TaskItem()
+        task2.name = "Buy Cheese!"
+        task2.imporant = true
+        
+        let task3 = TaskItem()
+        task3.name = "Mow the lawn"
+        task3.imporant = false
+        
+        return [task1, task2, task3]
+    }
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
 }
 
